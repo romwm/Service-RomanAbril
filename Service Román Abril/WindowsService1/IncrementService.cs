59e1985dbe6af8c002819e8b1ee134483b45eb7f
romwm <73566034+romwm@users.noreply.github.com>
2023-08-25 11:59:06 -0300

    Add files via upload

---
+++ Service Román Abril/WindowsService1/IncrementService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace WindowsService1
+{
+    public class IncrementService : ServiceBase
+    {
+        private Timer timer;
+        private int counter;
+
+        public IncrementService()
+        {
+            this.ServiceName = "IncrementService";
+            this.CanStop=true;
+            this.CanPauseAndContinue= true;
+            this.AutoLog=true;
+        }
+        protected override void OnStart(string[] args)
+        {
+            counter = 0;
+            string path = @"E:\Increment.txt";
+            if (!File.Exists(path)) 
+            {
+            File.Create(path).Close();
+            }
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += new ElapsedEventHandler(OnTimer);
+            timer.Start();
+        }
+        protected override void OnStop()
+        {
+            timer.Stop();
+        }
+        private void OnTimer(object sender, ElapsedEventArgs e) 
+        {
+            counter++;
+            string path = @"E:\IncrementService.txt";
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(counter);
+            }
+        }
+    }
+}
